48d8ecee772fb00217622acd8a43e88bdc11ae06
AziMtmd <88056975+AziMtmd@users.noreply.github.com>
2024-01-10 02:55:56 -0500

    Update run.sh slim

--- run.sh
+++ run.sh
@@ -6,16 +6,7 @@ pip install tf_slim
 #pip install tensorflow-datasets
 #pip install tfds-nightly
 python3 run.py
-import os
-folder_path = '/azi'
-for root, dirs, files in os.walk(folder_path):
-  for file in files:
-    if file.startswith('ckpt'):
-      os.remove(os.path.join(root, file))
-    if file.startswith('events'):
-      os.remove(os.path.join(root, file))
-    if file.startswith('checkpoint'):
-      os.remove(os.path.join(root, file))
+
 echo "Ending of the script";
 
 cat run.sh
