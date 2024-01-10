f8abba60abd14a058c292b2f74299e0d4ea0c1bf
AziMtmd <88056975+AziMtmd@users.noreply.github.com>
2024-01-09 23:48:49 -0500

    Update run.sh command to delete files added

--- run.sh
+++ run.sh
@@ -1,11 +1,20 @@
 #script1.sh
 clear
 echo "Starting shell script .."
-pip install tensorflow
-pip install tensorflow-datasets
+#pip install tensorflow
+#pip install tensorflow-datasets
 #pip install tfds-nightly
 python3 run.py
-
+import os
+folder_path = './azi'
+for root, dirs, files in os.walk(folder_path):
+  for file in files:
+    if file.startswith('ckpt'):
+      os.remove(os.path.join(root, file))
+    if file.startswith('events'):
+      os.remove(os.path.join(root, file))
+    if file.startswith('checkpoint'):
+      os.remove(os.path.join(root, file))
 echo "Ending of the script";
 
 cat run.sh
