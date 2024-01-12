f831507b898b664df1c19e97aa7ee1b13439cf45
AziMtmd <88056975+AziMtmd@users.noreply.github.com>
2024-01-12 16:44:58 -0500

    Update run.sh update

--- run.sh
+++ run.sh
@@ -5,8 +5,37 @@ pip install tf_slim
 #pip install tensorflow
 #pip install tensorflow-datasets
 #pip install tfds-nightly
+python3 M2/run.py --train_epochs=10 --m2_epoch=100
 python3 pakkon.py
-python3 M3/run.py
+python3 M2/run.py --train_epochs=10 --m2_epoch=80
+python3 pakkon.py
+python3 M2/run.py --train_epochs=5 --m2_epoch=90
+python3 pakkon.py
+python3 M2/run.py --train_epochs=5 --m2_epoch=100
+python3 pakkon.py
+python3 M2/run.py --train_epochs=5 --m2_epoch=80
+python3 pakkon.py
+python3 M2/run.py --train_epochs=15 --m2_epoch=90
+python3 pakkon.py
+python3 M2/run.py --train_epochs=15 --m2_epoch=100
+python3 pakkon.py
+python3 M2/run.py --train_epochs=15 --m2_epoch=80
+
+python3 M3/run.py --train_epochs=10 --m2_epoch=100
+python3 pakkon.py
+python3 M3/run.py --train_epochs=10 --m2_epoch=80
+python3 pakkon.py
+python3 M3/run.py --train_epochs=5 --m2_epoch=90
+python3 pakkon.py
+python3 M3/run.py --train_epochs=5 --m2_epoch=100
+python3 pakkon.py
+python3 M3/run.py --train_epochs=5 --m2_epoch=80
+python3 pakkon.py
+python3 M3/run.py --train_epochs=15 --m2_epoch=90
+python3 pakkon.py
+python3 M3/run.py --train_epochs=15 --m2_epoch=100
+python3 pakkon.py
+python3 M3/run.py --train_epochs=15 --m2_epoch=80
 
 echo "Ending of the script";
 
