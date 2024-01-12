088aa633bc18df0ccdd282ddc689a975223f89e5
AziMtmd <88056975+AziMtmd@users.noreply.github.com>
2024-01-12 17:15:30 -0500

    Update run.sh update

--- run.sh
+++ run.sh
@@ -47,13 +47,13 @@ python3 pakkon.py
 python3 M3/run.py --train_epochs=10 --m2_epoch=20 --m3_epoch=90 --temperature=0.3
 
 python3 pakkon.py
-python3 M4/run.py --train_epochs=10 --m2_epoch=40 --m3_epoch=90
+python3 M4/run.py --train_epochs=10 --m2_epoch=20 --m3_epoch=30 --m4_epoch=100
 python3 pakkon.py
-python3 M4/run.py --train_epochs=15 --m2_epoch=90
+python3 M4/run.py --train_epochs=10 --m2_epoch=20 --m3_epoch=20 --m4_epoch=100
 python3 pakkon.py
-python3 M4/run.py --train_epochs=15 --m2_epoch=100
+python3 M4/run.py --train_epochs=10 --m2_epoch=20 --m3_epoch=40 --m4_epoch=100
 python3 pakkon.py
-python3 M4/run.py --train_epochs=15 --m2_epoch=80
+python3 M4/run.py --train_epochs=10 --m2_epoch=20 --m3_epoch=20 --m4_epoch=120
 
 echo "Ending of the script";
 
