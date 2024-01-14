a1cccc92f9853feb9b67ebfa93e2eb275a03caf1
AziMtmd <88056975+AziMtmd@users.noreply.github.com>
2024-01-13 21:58:08 -0500

    Update run.sh done with 2M

--- run.sh
+++ run.sh
@@ -8,24 +8,14 @@ pip install model-profiler
 #pip install tfds-nightly
 
 python3 pakkon.py
-python3 M2/run.py --train_epochs=20 --m2_epoch=90 --learning_rate=0.3 --temperature=0.5
+python3 M2/run.py --train_epochs=20 --m2_epoch=90 --learning_rate=0.3 --temperature=0.6
+python3 pakkon.py
+python3 M2/run.py --train_epochs=20 --m2_epoch=100 --learning_rate=0.3 --temperature=0.6
+python3 pakkon.py
+python3 M2/run.py --train_epochs=5 --m2_epoch=90 --learning_rate=0.3 --temperature=0.6
+python3 pakkon.py
+python3 M2/run.py --train_epochs=5 --m2_epoch=100 --learning_rate=0.3 --temperature=0.6
 
-python3 pakkon.py
-python3 M2/run.py --train_epochs=10 --m2_epoch=100
-python3 pakkon.py
-python3 M2/run.py --train_epochs=5 --m2_epoch=90
-python3 pakkon.py
-python3 M2/run.py --train_epochs=5 --m2_epoch=100
-python3 pakkon.py
-python3 M2/run.py --train_epochs=15 --m2_epoch=90
-python3 pakkon.py
-python3 M2/run.py --train_epochs=15 --m2_epoch=100
-python3 pakkon.py
-python3 M2/run.py --train_epochs=15 --m2_epoch=80
-python3 pakkon.py
-python3 M2/run.py --train_epochs=5 --m2_epoch=110
-python3 pakkon.py
-python3 M2/run.py --train_epochs=10 --m2_epoch=120
 
 python3 pakkon.py
 python3 M3/run.py --train_epochs=10 --m2_epoch=40 --m3_epoch=90
