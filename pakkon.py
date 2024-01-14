fb638560960d3429edabc940347cac50215f658d
AziMtmd <88056975+AziMtmd@users.noreply.github.com>
2024-01-13 21:25:55 -0500

    Update pakkon.py updated

--- pakkon.py
+++ pakkon.py
@@ -1,11 +1,15 @@
 import os
-
 folder_path = '/azi'
 for root, dirs, files in os.walk(folder_path):
-  for file in files:
-    if file.startswith('ckpt'):
-      os.remove(os.path.join(root, file))
-    if file.startswith('events'):
-      os.remove(os.path.join(root, file))
-    if file.startswith('checkpoint'):
-      os.remove(os.path.join(root, file))
+    for file in files:
+        if file.startswith('ckpt'):
+            os.remove(os.path.join(root, file))
+        if file.startswith('events'):
+            os.remove(os.path.join(root, file))
+        if file.startswith('checkpoint'):
+            os.remove(os.path.join(root, file))
+        if file.startswith('flags'):
+            os.remove(os.path.join(root, file))            
+        if file.startswith('result'):
+            os.remove(os.path.join(root, file))  
+# os.kill(os.getpid(), 9)
