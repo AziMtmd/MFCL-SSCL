7a811df51524646009495d9b843c18843be46908
AziMtmd <88056975+AziMtmd@users.noreply.github.com>
2024-01-16 16:49:14 -0500

    Update run.sh Tuesday

--- run.sh
+++ run.sh
@@ -8,34 +8,34 @@ pip install model-profiler
 #pip install tfds-nightly
 
 
-python3 pakkon.py
-python3 M1/run.py --train_epochs=100 --learning_rate=0.6 --temperature=0.3
-python3 pakkon.py
-python3 M1/run.py --train_epochs=100 --learning_rate=0.6 --temperature=0.4
-python3 pakkon.py
-python3 M1/run.py --train_epochs=100 --learning_rate=0.5 --temperature=0.3
-python3 pakkon.py
-python3 M1/run.py --train_epochs=100 --learning_rate=0.5 --temperature=0.4
+#python3 pakkon.py
+#python3 M1/run.py --train_epochs=100 --learning_rate=0.6 --temperature=0.3
+#python3 pakkon.py
+#python3 M1/run.py --train_epochs=100 --learning_rate=0.6 --temperature=0.4
+#python3 pakkon.py
+#python3 M1/run.py --train_epochs=100 --learning_rate=0.5 --temperature=0.3
+#python3 pakkon.py
+#python3 M1/run.py --train_epochs=100 --learning_rate=0.5 --temperature=0.4
 python3 pakkon.py
 python3 M1/run.py --train_epochs=100 --learning_rate=0.7 --temperature=0.3
 python3 pakkon.py
 python3 M1/run.py --train_epochs=100 --learning_rate=0.8 --temperature=0.3
-python3 pakkon.py
-python3 M1/run.py --train_epochs=100 --learning_rate=1 --temperature=0.3
-python3 pakkon.py
-python3 M1/run.py --train_epochs=100 --learning_rate=1.5 --temperature=0.3
+#python3 pakkon.py
+#python3 M1/run.py --train_epochs=100 --learning_rate=1 --temperature=0.3
+#python3 pakkon.py
+#python3 M1/run.py --train_epochs=100 --learning_rate=1.5 --temperature=0.3
 
 
-python3 pakkon.py
-python3 M2/run.py --train_epochs=100 --m2_epoch=5 --learning_rate=0.5 --temperature=0.3
+#python3 pakkon.py
+#python3 M2/run.py --train_epochs=100 --m2_epoch=5 --learning_rate=0.5 --temperature=0.3
 python3 pakkon.py
 python3 M2/run.py --train_epochs=100 --m2_epoch=15 --learning_rate=0.7 --temperature=0.3
 python3 pakkon.py
 python3 M2/run.py --train_epochs=100 --m2_epoch=15 --learning_rate=0.8 --temperature=0.3
-python3 pakkon.py
-python3 M2/run.py --train_epochs=100 --m2_epoch=15 --learning_rate=1 --temperature=0.3
-python3 pakkon.py
-python3 M2/run.py --train_epochs=100 --m2_epoch=15 --learning_rate=1.5 --temperature=0.3
+#python3 pakkon.py
+#python3 M2/run.py --train_epochs=100 --m2_epoch=15 --learning_rate=1 --temperature=0.3
+#python3 pakkon.py
+#python3 M2/run.py --train_epochs=100 --m2_epoch=15 --learning_rate=1.5 --temperature=0.3
 
 
 
