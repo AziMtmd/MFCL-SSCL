f799fac950268da96c1dae7ce9b1f6f090b90af3
AziMtmd <88056975+AziMtmd@users.noreply.github.com>
2024-01-24 21:37:20 -0500

    Update run.sh FedSimCLR

--- run.sh
+++ run.sh
@@ -7,31 +7,8 @@ pip install model-profiler
 #pip install tensorflow-datasets
 #pip install tfds-nightly
 
-
 python3 pakkon.py
-python3 M1/run.py --train_epochs=25 --m2_epoch=100 --learning_rate=1.5 --temperature=0.3
-python3 pakkon.py
-python3 M1/run.py --train_epochs=30 --m2_epoch=100 --learning_rate=1.5 --temperature=0.3
-python3 pakkon.py
-python3 M1/run.py --train_epochs=35 --m2_epoch=100 --learning_rate=1.5 --temperature=0.3
-python3 pakkon.py
-python3 M1/run.py --train_epochs=40 --m2_epoch=100 --learning_rate=1.5 --temperature=0.3
-
-python3 pakkon.py
-python3 M1/run.py --train_epochs=25 --m2_epoch=105 --learning_rate=1.5 --temperature=0.3
-python3 pakkon.py
-python3 M1/run.py --train_epochs=30 --m2_epoch=110 --learning_rate=1.5 --temperature=0.3
-python3 pakkon.py
-python3 M1/run.py --train_epochs=35 --m2_epoch=115 --learning_rate=1.5 --temperature=0.3
-python3 pakkon.py
-python3 M1/run.py --train_epochs=40 --m2_epoch=115 --learning_rate=1.5 --temperature=0.3
-python3 pakkon.py
-python3 M1/run.py --train_epochs=25 --m2_epoch=110 --learning_rate=1.5 --temperature=0.3
-python3 pakkon.py
-python3 M1/run.py --train_epochs=30 --m2_epoch=105 --learning_rate=1.5 --temperature=0.3
-
-
-
+python3 M4/run.py 
 
 echo "Ending of the script";
 
