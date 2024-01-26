216e3673648d4df4e52d734c8e5e743a31c07b30
AziMtmd <88056975+AziMtmd@users.noreply.github.com>
2024-01-26 18:57:58 -0500

    Update run.py

--- FedSimCLR/run.py
+++ FedSimCLR/run.py
@@ -43,9 +43,9 @@ flags.DEFINE_integer('train_batch_size', 52, 'Batch size for training.')
 
 flags.DEFINE_integer('numofclients', 10, 'Number of epochs to train for.')
 
-flags.DEFINE_integer('train_epochs', 1, 'Number of epochs to train for.')
+flags.DEFINE_integer('train_epochs', 100, 'Number of epochs to train for.')
 
-flags.DEFINE_float('warmup_epochs', 1, 'Number of epochs of warmup.')
+flags.DEFINE_float('warmup_epochs', 10, 'Number of epochs of warmup.')
 
 flags.DEFINE_string('dataset', 'cifar10', 'Name of a dataset.')
 
