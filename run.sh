c77e1e14194031629674a58ddd27a5b71bfae93c
AziMtmd <88056975+AziMtmd@users.noreply.github.com>
2024-01-26 22:55:47 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,13 +8,13 @@ pip install model-profiler
 #pip install tfds-nightly
 
 python3 pakkon.py
-python3 FedSimCLR/run.py --dataset=cifar10 --image_size=32 --evel_aplit=test
+python3 FedSimCLR/run.py --dataset='cifar10' --image_size=32 --evel_aplit='test'
 
 python3 pakkon.py
-python3 FedSimCLR/run.py --dataset=cifar100 --image_size=32 --evel_aplit=test
+python3 FedSimCLR/run.py --dataset='cifar100' --image_size=32 --evel_aplit='test'
 
 python3 pakkon.py
-python3 FedSimCLR/run.py --dataset=imagenet_resized/64x64 --image_size=64 --evel_aplit=validation
+python3 FedSimCLR/run.py --dataset='imagenet_resized/64x64' --image_size=64 --evel_aplit='validation'
 
 echo "Ending of the script";
 
