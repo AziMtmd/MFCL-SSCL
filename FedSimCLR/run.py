72041e9fc94d82a481ec98df32623cd37120a2c0
AziMtmd <88056975+AziMtmd@users.noreply.github.com>
2024-01-26 19:27:08 -0500

    Update run.py

--- FedSimCLR/run.py
+++ FedSimCLR/run.py
@@ -47,7 +47,7 @@ flags.DEFINE_integer('train_epochs', 100, 'Number of epochs to train for.')
 
 flags.DEFINE_float('warmup_epochs', 10, 'Number of epochs of warmup.')
 
-flags.DEFINE_string('dataset', 'cifar10', 'Name of a dataset.')
+flags.DEFINE_string('dataset', 'imagenet_resized', 'Name of a dataset.')
 
 flags.DEFINE_integer('proj_out_dim', 128,'Number of head projection dimension.')
 
