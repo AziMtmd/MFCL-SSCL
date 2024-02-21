c906f8d59cd58b9f5b9566a07af60aa938de6abf
AziMtmd <88056975+AziMtmd@users.noreply.github.com>
2024-02-21 16:07:57 -0500

    Update run.sh s was missing

--- run.sh
+++ run.sh
@@ -8,13 +8,13 @@ pip install model-profiler
 #pip install tfds-nightly
 
 python3 pakkon.py
-python3 MFCL/run.py --dataset='cifar10' --image_size=32 --eval_split='test'
+python3 MFCLs/run.py --dataset='cifar10' --image_size=32 --eval_split='test'
 
 python3 pakkon.py
-python3 MFCL/run.py --dataset='cifar100' --image_size=32 --eval_split='test'
+python3 MFCLs/run.py --dataset='cifar100' --image_size=32 --eval_split='test'
 
 python3 pakkon.py
-python3 MFCL/run.py --dataset='imagenet_resized/64x64' --image_size=64 --eval_split='validation'
+python3 MFCLs/run.py --dataset='imagenet_resized/64x64' --image_size=64 --eval_split='validation'
 
 echo "Ending of the script";
 
