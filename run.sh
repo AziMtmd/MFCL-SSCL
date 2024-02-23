ffc4478e1be998518c702e2a83177474f67817f6
AziMtmd <88056975+AziMtmd@users.noreply.github.com>
2024-02-22 19:36:22 -0500

    Update run.sh second round

--- run.sh
+++ run.sh
@@ -7,14 +7,23 @@ pip install model-profiler
 #pip install tensorflow-datasets
 #pip install tfds-nightly
 
-python3 pakkon.py
-python3 MFCLs/run.py --dataset='cifar10' --image_size=32 --eval_split='test'
+#python3 pakkon.py
+#python3 MFCLs/run.py --dataset='cifar10' --image_size=32 --eval_split='test'
 
 python3 pakkon.py
-python3 MFCLs/run.py --dataset='cifar100' --image_size=32 --eval_split='test'
+python3 M1/run.py --dataset='cifar10' --image_size=32 --eval_split='test'
+
+#python3 pakkon.py
+#python3 MFCLs/run.py --dataset='cifar100' --image_size=32 --eval_split='test'
 
 python3 pakkon.py
-python3 MFCLs/run.py --dataset='imagenet_resized/64x64' --image_size=64 --eval_split='validation'
+python3 M1/run.py --dataset='cifar100' --image_size=32 --eval_split='test'
+
+#python3 pakkon.py
+#python3 MFCLs/run.py --dataset='imagenet_resized/64x64' --image_size=64 --eval_split='validation'
+
+python3 pakkon.py
+python3 M1/run.py --dataset='imagenet_resized/64x64' --image_size=64 --eval_split='validation'
 
 echo "Ending of the script";
 
