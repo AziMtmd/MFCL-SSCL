a5c1287ef42cbb00378e28a87af3a0c6f376d36e
AziMtmd <88056975+AziMtmd@users.noreply.github.com>
2024-03-20 22:30:47 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -9,14 +9,21 @@ pip install model-profiler
 #pip install tensorflow-datasets
 #pip install tfds-nightly
 
-#python3 pakkon.py
-#python3 M1/run.py --dataset='cifar10' --image_size=32 --eval_split='test'
-#python3 pakkon.py
-#python3 MFCLs/run.py --dataset='imagenet_resized/64x64' --image_size=64 --eval_split='validation'
-#python3 pakkon.py
-#python3 M1/run.py --dataset='imagenet_resized/64x64' --image_size=64 --eval_split='validation'
+python3 pakkon.py
+python3 M1/run.py --dataset='cifar10' --image_size=32 --eval_split='test' 350
+python3 pakkon.py
+python3 M1/run.py --dataset='cifar10' --image_size=32 --eval_split='test' 400
+python3 pakkon.py
+python3 M1/run.py --dataset='cifar10' --image_size=32 --eval_split='test' 450
+python3 pakkon.py
+python3 M1/run.py --dataset='cifar10' --image_size=32 --eval_split='test' 500
 
-python3 MF/run.py --color_jitter_strength=0.5 --train_batch_size=320 --train_epochs=400 --learning_rate=1.5 --weight_decay=1e-4 --temperature=0.3 --dataset=cifar10 --image_size=32 --eval_split=test --use_blur=False --resnet_depth=18 --model_dir=/tmp/simclr_test --tekrar=500 --NumofWorkers=10  --epochsm=1 --serBatch=32 --AutoBatch=64 --testBatch=320 --norm=GN --CPC=10
+python3 pakkon.py
+python3 MFCLs/run.py --dataset='imagenet_resized/64x64' --image_size=64 --eval_split='validation'
+python3 pakkon.py
+python3 M1/run.py --dataset='imagenet_resized/64x64' --image_size=64 --eval_split='validation'
+
+#python3 MF/run.py --color_jitter_strength=0.5 --train_batch_size=320 --train_epochs=400 --learning_rate=1.5 --weight_decay=1e-4 --temperature=0.3 --dataset=cifar10 --image_size=32 --eval_split=test --use_blur=False --resnet_depth=18 --model_dir=/tmp/simclr_test --tekrar=500 --NumofWorkers=10  --epochsm=1 --serBatch=32 --AutoBatch=64 --testBatch=320 --norm=GN --CPC=10
 
 
 echo "Ending of the script";
