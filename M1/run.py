5daa37a13b83dc08e82a2b724dac2b9aa7f94e35
AziMtmd <88056975+AziMtmd@users.noreply.github.com>
2024-04-01 18:35:28 -0400

    Update run.py

--- M1/run.py
+++ M1/run.py
@@ -37,7 +37,7 @@ from tensorflow.python.profiler.option_builder import ProfileOptionBuilder
 FLAGS = flags.FLAGS
 
 flags.DEFINE_integer('train_batch_size', 128, 'Batch size for training.')
-flags.DEFINE_integer('train_epochs', 1, 'Number of epochs to train for.')
+flags.DEFINE_integer('train_epochs', 80, 'Number of epochs to train for.')
 flags.DEFINE_float('warmup_epochs', 10, 'Number of epochs of warmup.')
 flags.DEFINE_string('dataset', 'cifar10', 'Name of a dataset.')
 flags.DEFINE_integer('proj_out_dim', 128,'Number of head projection dimension.')
@@ -49,7 +49,7 @@ flags.DEFINE_float('learning_rate', 1.5, 'Initial learning rate per batch size o
 flags.DEFINE_enum('learning_rate_scaling', 'linear', ['linear', 'sqrt'],'How to scale the learning rate as a function of batch size.')
 flags.DEFINE_float('weight_decay', 1e-6, 'Amount of weight decay to use.')
 flags.DEFINE_float('batch_norm_decay', 0.9, 'Batch norm decay parameter.')
-flags.DEFINE_string('train_split', 'train[0:5000]', 'Split for training.')
+flags.DEFINE_string('train_split', 'train', 'Split for training.')
 flags.DEFINE_integer('train_steps', 0, 'Number of steps to train for. If provided, overrides train_epochs.')
 flags.DEFINE_integer('eval_steps', 0, 'Number of steps to eval for. If not provided, evals over entire dataset.')
 flags.DEFINE_integer('eval_batch_size', 256, 'Batch size for eval.')
@@ -447,12 +447,7 @@ def main(argv):
       perform_evaluation(model, builder, eval_steps, checkpoint_manager.latest_checkpoint, strategy,topology)
 
 if __name__ == '__main__':
-  print('Shoroo Barname')
   tf.compat.v1.enable_v2_behavior()
   # For outside compilation of summaries on TPU.
   tf.config.set_soft_device_placement(True)
   app.run(main)
-
-
-
-
