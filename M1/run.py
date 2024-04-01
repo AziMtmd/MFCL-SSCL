27808ade6c404005066739d4b7e493e7b39a4cda
AziMtmd <88056975+AziMtmd@users.noreply.github.com>
2024-04-01 17:51:39 -0400

    Update run.py

--- M1/run.py
+++ M1/run.py
@@ -36,7 +36,7 @@ from tensorflow.python.profiler.option_builder import ProfileOptionBuilder
 
 FLAGS = flags.FLAGS
 
-flags.DEFINE_integer('train_batch_size', 64, 'Batch size for training.')
+flags.DEFINE_integer('train_batch_size', 128, 'Batch size for training.')
 flags.DEFINE_integer('train_epochs', 1, 'Number of epochs to train for.')
 flags.DEFINE_float('warmup_epochs', 10, 'Number of epochs of warmup.')
 flags.DEFINE_string('dataset', 'cifar10', 'Name of a dataset.')
