c8cabf3e69124eb27ac8ab2b754cdeb5d4b42452
AziMtmd <88056975+AziMtmd@users.noreply.github.com>
2024-04-23 02:04:52 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -19,10 +19,11 @@ pip install scikit-learn
 
 #python3 pakkon.py
 #python3 M4SemiParallel/run.py --dataset='cifar10' --image_size=32 --eval_split='test' 
-
 python3 pakkon.py
-python3 M1/run.py --dataset='cifar10' --image_size=32 --eval_split='test' --train_epochs=300
-python3 M1/run.py --dataset='cifar10' --image_size=32 --eval_split='test' --train_epochs=400 
+python3 M1/run.py --dataset='cifar10' --image_size=32 --eval_split='test' --train_epochs=100 --resnet_depth=50
+python3 M1/run.py --dataset='cifar10' --image_size=32 --eval_split='test' --train_epochs=200 --resnet_depth=50
+python3 M1/run.py --dataset='cifar10' --image_size=32 --eval_split='test' --train_epochs=300 --resnet_depth=50
+python3 M1/run.py --dataset='cifar10' --image_size=32 --eval_split='test' --train_epochs=400 --resnet_depth=50
 
 
 
