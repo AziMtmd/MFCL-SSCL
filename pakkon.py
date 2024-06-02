c618234d14f5e329663167e2c88a482dcf694784
AziMtmd <88056975+AziMtmd@users.noreply.github.com>
2024-06-02 14:49:41 -0400

    Update pakkon.py

--- pakkon.py
+++ pakkon.py
@@ -13,3 +13,17 @@ for root, dirs, files in os.walk(folder_path):
         if file.startswith('result'):
             os.remove(os.path.join(root, file))  
 # os.kill(os.getpid(), 9)
+
+import shutil
+import os
+
+# Replace 'folder_path' with the path to the folder you want to delete
+folder_path = '/content/tensor_data'
+
+# Check if the folder exists
+if os.path.exists(folder_path):
+    # Delete the folder and its contents
+    shutil.rmtree(folder_path)
+    print(f"{folder_path} has been deleted.")
+else:
+    print(f"{folder_path} does not exist.")
