8d78b4ad17f467cd4a9614e72142d7f618849ee1
AziMtmd <88056975+AziMtmd@users.noreply.github.com>
2024-07-23 22:09:41 -0400

    Update model_util.py

--- model_util.py
+++ model_util.py
@@ -120,3 +120,116 @@ for j in range(3,7):
     img_cropped.save(output_path)
 
   print(f"Cropped image saved to {output_path}")
+
+
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Create data points
+x = [1, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+y1 = [0.879435964, 0.772041462, 0.76, 0.753093858, 0.777272634, 0.820359571, 0.799020191, 0.828176420, 0.828921116, 0.823678286, 0.82]
+y2 = [0.819356398, 0.735595931, 0.5, 0.442329244, 0.445884135, 0.552466366, 0.667771164, 0.512706362, 0.605532057, 0.635901363, 0.65]
+y3 = [0.892363686, 0.774542635, 0.772, 0.771815673, 0.802712270, 0.817614449, 0.843992111, 0.845087226, 0.834253687, 0.851285629, 0.855]
+y4 = [0.855461708, 0.777957257, 0.76, 0.740030975, 0.739243372, 0.789641356, 0.827614284, 0.832840704, 0.778441363, 0.813524999, 0.83]
+# Plot the curve
+plt.figure(figsize=(5, 3))
+plt.plot(x, y1, label='(S-S, C-B)', color='b', linewidth=2, linestyle='-')
+plt.plot(x, y2, label='(S-S, M-B)', color='r', linewidth=2, linestyle='-')
+plt.plot(x, y3, label='(S-S, C-R)', color='g', linewidth=2, linestyle='-')
+plt.plot(x, y4, label='(S-S, M-R)', color='pink', linewidth=2, linestyle='-')
+#plt.fill_between(x, y, color='skyblue', alpha=0.4)
+#plt.annotate('Peak', xy=(np.pi/2, 1), xytext=(np.pi/2+1, 1.5),arrowprops=dict(facecolor='black', shrink=0.05))
+#plt.title('MI', fontsize=16); 
+plt.xlabel('Epochs', fontsize=14); plt.ylabel('MI', fontsize=14)
+plt.grid(True, which='both', linestyle='--', linewidth=0.5); plt.legend()
+plt.xticks(fontsize=12); plt.yticks(fontsize=12)
+plt.savefig('MI.png', dpi=300, bbox_inches='tight')
+plt.show()
+
+x = [1, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+y5 = [0.228103933, 0.204770860, 0.207, 0.210969056, 0.219764855, 0.223947986, 0.220327106, 0.226250025, 0.224947253, 0.222044661, 0.221]
+y6 = [0.215670498, 0.193888730, 0.16, 0.137903527, 0.138865453, 0.164897391, 0.187884403, 0.160525711, 0.175874553, 0.180997424, 0.185]
+y7 = [0.231104106, 0.207411893, 0.21, 0.216994274, 0.224837886, 0.223927784, 0.231950101, 0.228348051, 0.225701228, 0.228766458, 0.23]
+y8 = [0.223999570, 0.208828875, 0.206, 0.205115589, 0.208729075, 0.215564944, 0.223791858, 0.225194678, 0.214021856, 0.221820797, 0.22]
+# Plot the curve
+plt.figure(figsize=(5, 3))
+plt.plot(x, y5, label='(S-S, C-B)', color='b', linewidth=2, linestyle='-')
+plt.plot(x, y6, label='(S-S, M-B)', color='r', linewidth=2, linestyle='-')
+plt.plot(x, y7, label='(S-S, C-R)', color='g', linewidth=2, linestyle='-')
+plt.plot(x, y8, label='(S-S, M-R)', color='pink', linewidth=2, linestyle='-')
+#plt.fill_between(x, y, color='skyblue', alpha=0.4)
+#plt.annotate('Peak', xy=(np.pi/2, 1), xytext=(np.pi/2+1, 1.5),arrowprops=dict(facecolor='black', shrink=0.05))
+#plt.title('NMI', fontsize=16); 
+plt.xlabel('Epochs', fontsize=14); plt.ylabel('NMI', fontsize=14)
+plt.grid(True, which='both', linestyle='--', linewidth=0.5); plt.legend()
+plt.xticks(fontsize=12); plt.yticks(fontsize=12)
+plt.savefig('NMI.png', dpi=300, bbox_inches='tight')
+plt.show()
+
+x = [1, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+y9 = [0.197657662, 0.151700734, 0.1518, 0.152233441, 0.145865458, 0.14574933, 0.168335308, 0.17574204, 0.168755885, 0.167423425, 0.166]
+y10 = [0.11396199, 0.032357127, 0.0318, 0.030887462, 0.058712009, 0.047480333, 0.052714985, 0.052710766, 0.04816704, 0.038072031, 0.04]
+y11 = [0.217047742, 0.168694815, 0.175, 0.195172613, 0.182689776, 0.20221198, 0.20411218, 0.19931103, 0.186594711, 0.180037236, 0.176]
+y12 = [0.200723693, 0.189122083, 0.185, 0.182112407, 0.178829644, 0.18601723, 0.183698983, 0.194109563, 0.169843424, 0.175329776, 0.181]
+# Plot the curve
+plt.figure(figsize=(5, 3))
+plt.plot(x, y9, label='(S-S, C-B)', color='b', linewidth=2, linestyle='-')
+plt.plot(x, y10, label='(S-S, M-B)', color='r', linewidth=2, linestyle='-')
+plt.plot(x, y11, label='(S-S, C-R)', color='g', linewidth=2, linestyle='-')
+plt.plot(x, y12, label='(S-S, M-R)', color='pink', linewidth=2, linestyle='-')
+#plt.fill_between(x, y, color='skyblue', alpha=0.4)
+#plt.annotate('Peak', xy=(np.pi/2, 1), xytext=(np.pi/2+1, 1.5),arrowprops=dict(facecolor='black', shrink=0.05))
+#plt.title('Corr', fontsize=16); 
+plt.xlabel('Epochs', fontsize=14); plt.ylabel('Corr', fontsize=14)
+plt.grid(True, which='both', linestyle='--', linewidth=0.5); plt.legend()
+plt.xticks(fontsize=12); plt.yticks(fontsize=12)
+plt.savefig('Corr.png', dpi=300, bbox_inches='tight')
+plt.show()
+
+
+x = [1, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+yt1_2= [0.171875, 0.765625, 0.902344, 0.929688, 0.890625, 0.941406, 0.933594, 0.960938, 0.976562, 0.976562, 0.988281]
+yt1_3 = [0.140625, 0.195312, 0.414062, 0.367188, 0.382812, 0.292969, 0.25, 0.335938, 0.269531, 0.203125, 0.226562]
+yt1_4 = [0.125, 0.402344, 0.628906, 0.507812, 0.484375, 0.5625, 0.765625, 0.875, 0.871094, 0.917969, 0.917969]
+yt1_5 = [0.101562, 0.09375, 0.09375, 0.113281, 0.09375, 0.097656, 0.121094, 0.121094, 0.113281, 0.101562, 0.101562]
+yt1_6 = [0.113281, 0.085938, 0.09375, 0.078125, 0.105469, 0.09375, 0.101562, 0.109375, 0.101562, 0.101562, 0.101562]
+# Plot the curve
+plt.figure(figsize=(5, 3))
+plt.plot(x, yt1_2, label='(S-S)', color='purple', linewidth=2, linestyle='-')
+plt.plot(x, yt1_3, label='(C-B)', color='b', linewidth=2, linestyle='-')
+plt.plot(x, yt1_4, label='(M-B)', color='r', linewidth=2, linestyle='-')
+plt.plot(x, yt1_5, label='(C-R)', color='g', linewidth=2, linestyle='-')
+plt.plot(x, yt1_6, label='(M-R)', color='pink', linewidth=2, linestyle='-')
+#plt.fill_between(x, y, color='skyblue', alpha=0.4)
+#plt.annotate('Peak', xy=(np.pi/2, 1), xytext=(np.pi/2+1, 1.5),arrowprops=dict(facecolor='black', shrink=0.05))
+#plt.title('Corr', fontsize=16); 
+plt.xlabel('Epochs', fontsize=14); plt.ylabel('Top-1 Accuracy', fontsize=14)
+plt.grid(True, which='both', linestyle='--', linewidth=0.5); plt.legend()
+plt.xticks(fontsize=12); plt.yticks(fontsize=12)
+plt.savefig('Top-1.png', dpi=300, bbox_inches='tight')
+plt.show()
+
+
+x = [1, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+yt5_2= [0.691406, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+yt5_3 = [0.570312, 0.746094, 0.875, 0.871094, 0.902344, 0.90625, 0.941406, 0.90625, 0.957031, 0.957031, 0.960938]
+yt5_4 = [0.523438, 0.84375, 0.972656, 0.976562, 0.984375, 0.976562, 0.992188, 0.996094, 0.996094, 1, 1]
+yt5_5 = [0.492188, 0.496094, 0.488281, 0.5, 0.511719, 0.503906, 0.484375, 0.476562, 0.488281, 0.484375, 0.472656]
+yt5_6 = [0.460938, 0.429688, 0.46875, 0.542969, 0.519531, 0.523438, 0.550781, 0.523438, 0.570312, 0.574219, 0.574219]
+# Plot the curve
+plt.figure(figsize=(5, 3))
+plt.plot(x, yt5_2, label='(S-S)', color='purple', linewidth=2, linestyle='-')
+plt.plot(x, yt5_3, label='(C-B)', color='b', linewidth=2, linestyle='-')
+plt.plot(x, yt5_4, label='(M-B)', color='r', linewidth=2, linestyle='-')
+plt.plot(x, yt5_5, label='(C-R)', color='g', linewidth=2, linestyle='-')
+plt.plot(x, yt5_6, label='(M-R)', color='pink', linewidth=2, linestyle='-')
+#plt.fill_between(x, y, color='skyblue', alpha=0.4)
+#plt.annotate('Peak', xy=(np.pi/2, 1), xytext=(np.pi/2+1, 1.5),arrowprops=dict(facecolor='black', shrink=0.05))
+#plt.title('Corr', fontsize=16); 
+plt.xlabel('Epochs', fontsize=14); plt.ylabel('Top-5 Accuracy', fontsize=14)
+plt.grid(True, which='both', linestyle='--', linewidth=0.5); plt.legend()
+plt.xticks(fontsize=12); plt.yticks(fontsize=12)
+plt.savefig('Top-5.png', dpi=300, bbox_inches='tight')
+plt.show()
